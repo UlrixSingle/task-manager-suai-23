48a3b6c0303686328b4de4f54ca20505fc735d2b
UlrixSingle <yan.sdean@gmail.com>
2024-02-04 12:10:41 -0500

    Home page test 2010-04-02-2024

--- app/routes.py
+++ app/routes.py
@@ -12,9 +12,12 @@ def index():
     else:
         return "Unknown"
 
-@app.route('/admin_home')
-def test_connection():
+'''
+@app.route('/admin_home', methods=['GET','POST'])
+def get_admin_home():
     message = "Состояние неопределенно"
+    admin_system_role_id = 1
+    user_system_role_id = 2
     
     try:
         with psycopg.connect(host=app.config['DB_SERVER'], 
@@ -25,20 +28,92 @@ def test_connection():
                               connect_timeout=app.config['DB_TIMEOUT']) as con:
             cur = con.cursor()
             
-            user_names = cur.execute(f'SELECT "nickname" FROM "user";').fetchall()
-            return render_template('admin_home.html', 
-                                    admin_nickname='Penguin',
+            admin_names = cur.execute(f'SELECT "nickname" FROM "user" WHERE system_role_id = %s;', [1]).fetchall()
+            user_names = cur.execute(f'SELECT "nickname" FROM "user" WHERE system_role_id = %s;', [2]).fetchall()
+            return render_template('home.html',
+                                    system_role_id=1,
+                                    system_role = 'Администратор',
+                                    nickname='Penguin',
+                                    admin_names=admin_names,
                                     user_names=user_names)
         
     except Exception as e:
         message = f"Ошибка подключения: {e}"
         return message
+'''
+    
+@app.route('/user_home', methods=['GET','POST'])
+def get_home_user():
+    try:
+        with psycopg.connect(host=app.config['DB_SERVER'], 
+                              port=app.config['DB_PORT'],
+                              user=app.config['DB_USER'], 
+                              password=app.config['DB_PASSWORD'],
+                              dbname=app.config['DB_NAME'],
+                              connect_timeout=app.config['DB_TIMEOUT']) as con:
+            cur = con.cursor()
+            
+            admin_names = []
+            user_names = []
+            return render_template('home.html',
+                                    system_role_id=2,
+                                    system_role = 'Пользователь',
+                                    nickname='Knot',
+                                    admin_names=admin_names,
+                                    user_names=user_names,
+                                    project_names=[])
         
-    '''
     except Exception as e:
         message = f"Ошибка подключения: {e}"
-    else:
-        message = "Подключение успешно"
-    finally:
         return message
-    '''
+    
+    
+# Главная страница администратора
+@app.route('/admin_home', methods=['GET','POST'])
+def get_home_admin():
+    try:
+        with psycopg.connect(host=app.config['DB_SERVER'], 
+                              port=app.config['DB_PORT'],
+                              user=app.config['DB_USER'], 
+                              password=app.config['DB_PASSWORD'],
+                              dbname=app.config['DB_NAME'],
+                              connect_timeout=app.config['DB_TIMEOUT']) as con:
+            cur = con.cursor()
+            
+            admin_names = cur.execute(f'SELECT "nickname" FROM "user" WHERE system_role_id = %s;', [1]).fetchall()
+            user_names = cur.execute(f'SELECT "nickname" FROM "user" WHERE system_role_id = %s;', [2]).fetchall()
+            return render_template('home.html',
+                                    system_role_id=1,
+                                    system_role = 'Администратор',
+                                    nickname='Penguin',
+                                    admin_names=admin_names,
+                                    user_names=user_names)
+        
+    except Exception as e:
+        message = f"Ошибка подключения: {e}"
+        return message
+
+
+@app.route('/profile/<user_id>', methods=['GET','POST'])
+def get_profile( user_id):
+    try:
+        with psycopg.connect(host=app.config['DB_SERVER'], 
+                              port=app.config['DB_PORT'],
+                              user=app.config['DB_USER'], 
+                              password=app.config['DB_PASSWORD'],
+                              dbname=app.config['DB_NAME'],
+                              connect_timeout=app.config['DB_TIMEOUT']) as con:
+            cur = con.cursor()
+            
+            user = cur.execute(f'SELECT * FROM "user" WHERE "user_id" = %s;', [user_id]).fetchone()
+            return render_template('home.html',
+                                    system_role_id=2,
+                                    system_role = 'Пользователь',
+                                    nickname='Knot',
+                                    user=user)
+        
+    except Exception as e:
+        message = f"Ошибка подключения: {e}"
+        return message
+
+
