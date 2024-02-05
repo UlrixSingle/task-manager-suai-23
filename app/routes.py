b7ca6ad65aca7a9bc734433a649abe516569639b
UlrixSingle <yan.sdean@gmail.com>
2024-02-05 03:26:06 -0500

    Made forms for Home page 1125-05-02-2024

--- app/routes.py
+++ app/routes.py
@@ -42,35 +42,9 @@ def get_admin_home():
         return message
 '''
     
-@app.route('/user_home', methods=['GET','POST'])
-def get_home_user():
-    try:
-        with psycopg.connect(host=app.config['DB_SERVER'], 
-                              port=app.config['DB_PORT'],
-                              user=app.config['DB_USER'], 
-                              password=app.config['DB_PASSWORD'],
-                              dbname=app.config['DB_NAME'],
-                              connect_timeout=app.config['DB_TIMEOUT']) as con:
-            cur = con.cursor()
-            
-            admin_names = []
-            user_names = []
-            return render_template('home.html',
-                                    system_role_id=2,
-                                    system_role = 'Пользователь',
-                                    nickname='Knot',
-                                    admin_names=admin_names,
-                                    user_names=user_names,
-                                    project_names=[])
-        
-    except Exception as e:
-        message = f"Ошибка подключения: {e}"
-        return message
-    
-    
 # Главная страница администратора
-@app.route('/admin_home', methods=['GET','POST'])
-def get_home_admin():
+@app.route('/<cur_user_id>/home', methods=['GET','POST'])
+def home( cur_user_id):
     try:
         with psycopg.connect(host=app.config['DB_SERVER'], 
                               port=app.config['DB_PORT'],
@@ -80,22 +54,49 @@ def get_home_admin():
                               connect_timeout=app.config['DB_TIMEOUT']) as con:
             cur = con.cursor()
             
-            admin_names = cur.execute(f'SELECT "nickname" FROM "user" WHERE system_role_id = %s;', [1]).fetchall()
-            user_names = cur.execute(f'SELECT "nickname" FROM "user" WHERE system_role_id = %s;', [2]).fetchall()
+            system_role_admin_id = 1
+            system_role_user_id = 2
+            
+            cur_user = cur.execute(
+                f'SELECT "usr"."system_role_id", "usr"."name", "usr"."nickname", "usr"."user_id" FROM (select * from "user" natural full outer join "system_role") "usr" WHERE  "user_id" = %s;',
+                [cur_user_id]).fetchone()
+            system_role_id = cur_user[0]
+            system_role = cur_user[1]
+            nickname = cur_user[2]
+            cur_user_id = cur_user[3]
+            
+            admins = []
+            users = []
+            projects = []
+            if( system_role_id == 1):
+                admins = cur.execute(f'SELECT "user_id", "nickname" FROM "user" WHERE system_role_id = %s;', [system_role_admin_id]).fetchall()
+                users = cur.execute(f'SELECT "user_id", "nickname" FROM "user" WHERE system_role_id = %s;', [system_role_user_id] ).fetchall()
+                projects = cur.execute(f'SELECT "project_id", "name" FROM "project"').fetchall()
+            
             return render_template('home.html',
-                                    system_role_id=1,
-                                    system_role = 'Администратор',
-                                    nickname='Penguin',
-                                    admin_names=admin_names,
-                                    user_names=user_names)
+                                    system_role_id=system_role_id,
+                                    system_role = system_role,
+                                    nickname=nickname,
+                                    cur_user_id=cur_user_id,
+                                    admins=admins,
+                                    users=users,
+                                    projects=projects,
+                                    system_role_admin_id=system_role_admin_id)
         
     except Exception as e:
         message = f"Ошибка подключения: {e}"
         return message
+    
+@app.route('/<cur_user_id>/announce', methods=['GET','POST'])
+def announce( cur_user_id):
+    return 'OK'
+    
+@app.route('/<cur_user_id>/exit', methods=['GET','POST'])
+def exit( cur_user_id):
+    return 'OK'
 
-
-@app.route('/profile/<user_login>', methods=['GET','POST'])
-def get_profile( user_login):
+@app.route('/<cur_user_id>/profile/<user_id>', methods=['GET','POST'])
+def profile( cur_user_id, user_id):
     try:
         with psycopg.connect(host=app.config['DB_SERVER'], 
                               port=app.config['DB_PORT'],
@@ -104,20 +105,31 @@ def get_profile( user_login):
                               dbname=app.config['DB_NAME'],
                               connect_timeout=app.config['DB_TIMEOUT']) as con:
             cur = con.cursor()
+            
+            cur_user = cur.execute(
+                f'SELECT "usr"."system_role_id", "usr"."name", "usr"."nickname", "usr"."user_id" FROM (select * from "user" natural full outer join "system_role") "usr" WHERE  "user_id" = %s;',
+                [cur_user_id]).fetchone()
+            system_role_id = cur_user[0]
+            system_role = cur_user[1]
+            nickname = cur_user[2]
+            cur_user_id = cur_user[3]
             
             user = cur.execute(
-                f'SELECT "usr"."name", "usr"."nickname", "usr"."second_name", "usr"."first_name", "usr"."surname", "usr"."mail", "usr"."phone", "usr"."descr", "usr"."login" FROM (SELECT * FROM "system_role", "user" WHERE "user"."system_role_id" = "system_role"."system_role_id") "usr" WHERE  "login" = %s;',
-                [user_login]).fetchone()
-                
+                f'SELECT "usr"."name", "usr"."nickname", "usr"."second_name", "usr"."first_name", "usr"."surname", "usr"."mail", "usr"."phone", "usr"."descr", "usr"."user_id" FROM (select * from "user" natural full outer join "system_role") "usr" WHERE  "user_id" = %s;',
+                [user_id]).fetchone()
+            
             return render_template('profile.html',
-                                    system_role_id=2,
-                                    system_role = 'Пользователь',
-                                    nickname='Knot',
-                                    login='user2',
+                                    system_role_id=system_role_id,
+                                    system_role = system_role,
+                                    nickname=nickname,
+                                    cur_user_id=cur_user_id,
                                     user=user)
         
     except Exception as e:
         message = f"Ошибка подключения: {e}"
         return message
 
+@app.route('/<cur_user_id>/project/<project_id>', methods=['GET','POST'])
+def project( cur_user_id, project_id):
+    return 'OK'
 
